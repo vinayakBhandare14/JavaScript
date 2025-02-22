ac1f9b7e3244fc4d9f09b28f75e62b05dc94fae1
Vinayak Bhandare <vinayakbhandare2000@gmail.com>
2025-02-22 12:06:58 +0530

    Add files via upload

---
+++ arithmaticOperators.js
@@ -0,0 +1,30 @@
+
+
+//Arithmatic operators
+
+//using  +,_,*
+
+let x=3;
+let y=5.5;
+let z = (10+x)*y;
+console.log("Z  : "+z);
+
+// using x power of y (x^y)
+
+let base = 5;
+let power = 2;
+
+let result = base ** power;
+
+console.log("RESULT : "+result);
+
+// using pre - post Operators
+
+let a = 5;
+//prifix
+console.log("++a :: "+(++a));
+
+//postfix
+let b =5;
+console.log("b++ :: "+(b++));
+
